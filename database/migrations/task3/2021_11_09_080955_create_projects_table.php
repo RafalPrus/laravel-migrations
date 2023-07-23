7aea3612d6685fcfe4518089fe70198c8abb1848
Rafał Pruś <rafalprues@gmail.com>
2023-07-23 10:59:40 +0200

    Solve tests

--- database/migrations/task3/2021_11_09_080955_create_projects_table.php
+++ database/migrations/task3/2021_11_09_080955_create_projects_table.php
@@ -14,6 +14,7 @@ class CreateProjectsTable extends Migration
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
+            $table->softDeletes();
             $table->id();
             $table->string('name');
             $table->timestamps();
